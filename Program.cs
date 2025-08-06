6b0deedeee8f25bf14d6a55e522274b30bb34163
Beverly-itsme <zhuakibeverly7@gmail.com>
2025-08-06 12:24:34 +0200

    nova

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyFirstCSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My First C# Program!");
+
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("What year were you born? ");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - birthYear;
+
+            Console.WriteLine($"\nHello, {name}!");
+            Console.WriteLine($"You are approximately {age} years old.");
+        }
+    }
+}
+
